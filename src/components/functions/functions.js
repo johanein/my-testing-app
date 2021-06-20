add09837b38ea087880b423ee42df54fe7220268
Albert <johanein@gmail.com>
2021-06-20 23:20:13 +0530

    jest testing upto async in vanila

---
+++ src/components/functions/functions.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const functions = {
+  add: (sum1, sum2) => sum1 + sum2,
+  isnull: () => null,
+  checkValue: (x) => x,
+  createUser: () => {
+    const user = { name: "albert", lastname: "yohannan" };
+    return user;
+  },
+  fetchUsers: () =>
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/1")
+      .then((json) => json.data),
+};
+
+export default functions;
